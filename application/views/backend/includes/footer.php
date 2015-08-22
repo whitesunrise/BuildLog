55142120d344db767ae095130ecf24098b9860d3
Adam W <adam@whitesunrise.com>
2015-08-22 15:40:01 +0000

    Added Adminre Assets

--- application/views/backend/includes/footer.php
+++ application/views/backend/includes/footer.php
@@ -1,13 +1,13 @@
         <!-- START JAVASCRIPT SECTION (Load javascripts at bottom to reduce load time) -->
         <!-- Application and vendor script : mandatory -->
-        <script type="text/javascript" src="../../javascript/vendor.js"></script>
-        <script type="text/javascript" src="../../javascript/core.js"></script>
-        <script type="text/javascript" src="../../javascript/backend/app.js"></script>
+        <script type="text/javascript" src=".<?=base_url()?>assets/js/vendor.js"></script>
+        <script type="text/javascript" src="<?=base_url()?>assets/js/core.js"></script>
+        <script type="text/javascript" src="<?=base_url()?>assets/js/backend/app.js"></script>
         <!--/ Application and vendor script : mandatory -->
 
         <!-- Plugins and page level script : optional -->
-        <script type="text/javascript" src="../../plugins/parsley/js/parsley.js"></script>
-        <script type="text/javascript" src="../../javascript/backend/pages/login.js"></script>
+        <script type="text/javascript" src="<?=base_url()?>assets/js/parsley.js"></script>
+        <script type="text/javascript" src="<?=base_url()?>assets/js/login.js"></script>
         <!--/ Plugins and page level script : optional -->
 
     </body>
--- application/views/backend/includes/header.php
+++ application/views/backend/includes/header.php
@@ -10,29 +10,29 @@
         <meta name="keywords" content="admin, admin template, admin themes, bootstrap, Bootstrap 3, bower, cms, dashboard, flat, grunt, html template, Jekyll, minimal, premium admin templates, responsive">
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 
-        <link rel="apple-touch-icon-precomposed" sizes="144x144" href="../../image/touch/apple-touch-icon-144x144-precomposed.png">
-        <link rel="apple-touch-icon-precomposed" sizes="114x114" href="../../image/touch/apple-touch-icon-114x114-precomposed.png">
-        <link rel="apple-touch-icon-precomposed" sizes="72x72" href="../../image/touch/apple-touch-icon-72x72-precomposed.png">
-        <link rel="apple-touch-icon-precomposed" href="../../image/touch/apple-touch-icon-57x57-precomposed.png">
-        <link rel="shortcut icon" href="../../image/favicon.ico">
+        <link rel="apple-touch-icon-precomposed" sizes="144x144" href="<?=base_url()?>assets/image/touch/apple-touch-icon-144x144-precomposed.png">
+        <link rel="apple-touch-icon-precomposed" sizes="114x114" href="<?=base_url()?>assets/image/touch/apple-touch-icon-114x114-precomposed.png">
+        <link rel="apple-touch-icon-precomposed" sizes="72x72" href="<?=base_url()?>assets/image/touch/apple-touch-icon-72x72-precomposed.png">
+        <link rel="apple-touch-icon-precomposed" href="<?=base_url()?>assets/image/touch/apple-touch-icon-57x57-precomposed.png">
+        <link rel="shortcut icon" href="<?=base_url()?>assets/image/favicon.ico">
         <!--/ END META SECTION -->
 
         <!-- START STYLESHEETS -->
         <!-- Plugins stylesheet : optional -->
-        <link rel="stylesheet" href="../../plugins/bootstrap/css/bootstrap.css">
-        <link rel="stylesheet" href="../../plugins/animatecss/css/animate.css">
+        <link rel="stylesheet" href="<?=base_url()?>assets/css/bootstrap.css">
+        <link rel="stylesheet" href="<?=base_url()?>assets/css/animate.css">
         <!--/ Plugins stylesheet : optional -->
 
         <!-- Application stylesheet : mandatory -->
-        <link rel="stylesheet" href="../../stylesheet/layouts/layout.css">
-        <link rel="stylesheet" href="../../stylesheet/uielement.css">
+        <link rel="stylesheet" href="<?=base_url()?>assets/css/layout.css">
+        <link rel="stylesheet" href="<?=base_url()?>assets/css/uielement.css">
         <!--/ Application stylesheet -->
 
         <!-- Theme stylesheet : optional -->
         <!--/ Theme stylesheet : optional -->
 
         <!-- modernizr script -->
-        <script type="text/javascript" src="../../plugins/modernizr/js/modernizr.js"></script>
+        <script type="text/javascript" src="<?=base_url()?>assets/js/modernizr.js"></script>
         <!--/ modernizr script -->
         <!-- END STYLESHEETS -->
 
